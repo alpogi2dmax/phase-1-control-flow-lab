7450b2534acc50e2d2f51c11253e184eaf98e367
Al Cerdan Lico <alcerdan@yahoo.com>
2024-09-04 13:26:10 -0700

    completed control-flor-lab

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let result
+  if (feet > 2500) {
+    result = 'No can do.';
+  } else if (feet > 2000) {
+    result = 'I will gladly take your thirty bucks.';
+  } else if (feet > 400) {
+    result = 'That will be twenty bucks.';
+  } else if (feet <= 400) {
+    result = 'This one is on me!';
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let result = city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+
+  return result;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let result;
+
+  switch (tip) {
+    case 'generous':
+      result = "Thank you so much."
+      break;
+    case 'not as generous':
+      result = "Thank you."
+      break;
+    default:
+      result = "Bye.";
+  }
+  return result;
 }
